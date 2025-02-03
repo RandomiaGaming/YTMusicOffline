12859ebbe5f3fded38c0b46e028d379bf16d4e35
RandomiaGaming <randomiagaming@gmail.com>
2025-02-03 02:12:43 -0800

    Refactoring

---
+++ YTMusicHelper/GeneralHelper.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.Text;
+using System.IO;
+
+public static class GeneralHelper
+{
+    public static List<string> Split(string value, string separator)
+    {
+        List<string> output = new List<string>();
+        while (true)
+        {
+            int i = value.IndexOf(separator);
+            if (i == -1)
+            {
+                output.Add(value);
+                break;
+            }
+            else
+            {
+                output.Add(value.Substring(0, i));
+                value = value.Substring(i + separator.Length);
+
+                if (value.Length == 0)
+                {
+                    output.Add("");
+                    break;
+                }
+            }
+        }
+        return output;
+    }
+    public static void SaveJson<T>(T obj, string jsonFilePath)
+    {
+        string json = JsonConvert.SerializeObject(obj, Formatting.Indented);
+        File.WriteAllText(jsonFilePath, json, Encoding.UTF8);
+    }
+    public static T LoadJson<T>(string jsonFilePath)
+    {
+        string json = File.ReadAllText(jsonFilePath, Encoding.UTF8);
+        return JsonConvert.DeserializeObject<T>(json);
+    }
+}
---
+++ YTMusicHelper/YTDataTypes.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System;
+
+public sealed class VideoRelocation
+{
+    public string OriginalVideoID = null;
+    public string NewVideoID = null;
+}
+public sealed class Ghosts
+{
+    public List<string> RemovedVideoIDs = new List<string>();
+    public List<string> UnavailableVideoIDs = new List<string>();
+}
+public sealed class Song
+{
+    public string VideoID = null;
+    public string SongName = null;
+    public string AlbumName = null;
+    public string ArtistName = null;
+    public string[] FeaturedArtistNames = null;
+    public DateTime ReleaseDate = new DateTime();
+}
+public sealed class MusicDescription
+{
+    public string VideoID = null;
+    public string ProvidedBy = null;
+    public string SongName = null;
+    public List<string> ArtistNames = null;
+    public string AlbumName = null;
+    public List<string> PublishStatements = null;
+    public DateTime? ReleasedOn = null;
+    public List<Tuple<string, string>> RoleNamePairs = null;
+}
